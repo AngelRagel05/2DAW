291e0fa27fd698dfce4ab62ac57e4c079fb76ec3
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-10-02 20:53:23 +0200

    Implement code changes to enhance functionality and improve performance

---
+++ actividadesNoEvaluables_01/parte3/ConstructorFrasesCuñados.php
@@ -0,0 +1,51 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frases Cuñados</title>
+    <link rel="icon" href="img/cuñado.png">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+
+<body>
+    <form action="">
+        <label for="sujeto">Elige un sujeto:</label>
+        <select name="sujeto" id="sujeto">
+            <option value="Yo en tu lugar">Yo en tu lugar </option>
+            <option value="La verdad es que">La verdad es que</option>
+            <option value="Tú sabes que">Tú sabes que</option>
+            <option value="No hay duda de que">No hay duda de que</option>
+            <option value="Al final">Al final</option>
+        </select>
+
+        <br>
+
+        <label for="accion">Elige una acción:</label>
+        <select name="accion" id="accion">
+            <option value="siempre hay que invertir en"> siempre hay que invertir en </option>
+            <option value="lo importante es"> lo importante es </option>
+            <option value="nunca subestimes"> nunca subestimes </option>
+            <option value="yo siempre digo que"> yo siempre digo que </option>
+            <option value="el secreto está en"> el secreto está en </option>
+        </select>
+
+        <br>
+
+        <label for="cierre">Elige un cierre:</label>
+        <select name="cierre" id="cierre">
+            <option value="la bolsa de valores">la bolsa de valores</option>
+            <option value="el no tener dinero congelado en el banco">el no tener dinero congelado en el banco</option>
+            <option value="los chemtrails">los chemtrails</option>
+            <option value="la nube">la nube</option>
+            <option value="las criptocoins">las criptocoins</option>
+        </select>
+    </form>
+</body>
+
+</html>
