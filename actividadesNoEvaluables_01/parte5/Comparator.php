8ed0ce9f11d0c7d612c841a5121900aaff765f74
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-10-04 13:18:57 +0200

    Modifico el titulo de la página

--- actividadesNoEvaluables_01/parte5/Comparator.php
+++ actividadesNoEvaluables_01/parte5/Comparator.php
@@ -34,7 +34,7 @@ if ($operacion !== null) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Comparator</title>
+    <title>Calculadora / Comparador</title>
     <link rel="icon" href="img/calculadora.png">
     <link rel="stylesheet" href="../css/bootstrap.css">
 </head>
