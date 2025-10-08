7b961429617780b271d57ef68dcaf289ded689c4
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-10-08 11:55:50 +0200

    añado el favicon

--- Servidor/problemaMontyHall/montyHall.php
+++ Servidor/problemaMontyHall/montyHall.php
@@ -137,7 +137,7 @@ if (isset($_GET['accion'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Monty Hall</title>
-    <link rel="icon" href="img/logo.png">
+    <link rel="icon" href="img/ruleta.png">
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 
