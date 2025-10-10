12db8c7e8a9aeefc5c7882e020070837fe98bf3a
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-10-10 10:51:35 +0200

    añado creacion bolaExtraDeDragon

---
+++ Servidor/bolaExtraDeDragon/entrenamiento.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entrenamiento Son Goku</title>
+    <link rel="icon" href="img/ruleta.png">
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
