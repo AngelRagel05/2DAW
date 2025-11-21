f891edf4628eb5ad6ea928ed15e8f53de9801503
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2025-11-21 22:44:53 +0100

    Refactorizo app1 y app2

--- Clientes/unidad4/actividadEventos/app1.js
+++ Clientes/unidad4/actividadEventos/app1.js
@@ -1,44 +1,44 @@
 "use strict";
 
 function log(mensaje) {
-    const li = document.getElementById("log");
-    li.innerHTML += "<li>" + mensaje + "</li>"
+  const li = document.getElementById("log");
+  li.innerHTML += "<li>" + mensaje + "</li>";
 }
 
 const zona = document.getElementsById("zona-mouse");
 
 zona.addEventListener("mouseenter", () => {
-    zona.classList.add("highlight");
-    log("El ratón entrá en la zona sensible");
+  zona.classList.add("highlight");
+  log("El ratón entrá en la zona sensible");
 });
 
 zona.addEventListener("mouseLeave", () => {
-    zona.classList.remove("highlight");
-    log("El ratón salió de la zona sensible");
-})
+  zona.classList.remove("highlight");
+  log("El ratón salió de la zona sensible");
+});
 
 zona.addEventListener("click", () => {
-    log("Click");
+  log("Click");
 });
 
 zona.addEventListener("mousemove", (e) => {
-    log("Posición: " + e.clientX + ", " + e.clientY); 
+  log("Posición: " + e.clientX + ", " + e.clientY);
 });
 
 const input = document.getElementById("input-texto");
 
 input.addEventListener("focus", () => {
-    log("Input enfocado");
+  log("Input enfocado");
 });
 
 input.addEventListener("blur", () => {
-    log("Input desenfocado");
+  log("Input desenfocado");
 });
 
 input.addEventListener("keydown", (e) => {
-    log("Tecla pulsada: " + e.key);
+  log("Tecla pulsada: " + e.key);
 });
 
 input.addEventListener("keyup", (e) => {
-    log("Tecla soltada: " + e.code);
+  log("Tecla soltada: " + e.code);
 });
